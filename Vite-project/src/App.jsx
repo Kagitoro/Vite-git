3a1a2f4e6b0994ca1d4e63103bd6fc0eff79eb2d
Radion Duda <rodion.dooda@ukr.net>
2024-10-11 15:26:02 -0400

    first commit

---
+++ Vite-project/src/App.jsx
@@ -0,0 +1,36 @@
+import './reset.css';
+
+
+function App() {
+  return (
+    <>
+      <div className="container bg-slate-400 p-4 text-center">
+        <div className="menu text-2xl">
+          <ul className="menu__block flex gap-10    ">
+
+            <li className="menu__items">
+              <a href="#" className="menu__link flex   ">Home</a>
+            </li>
+
+            <li className="menu__items">
+              <a href="#" className="menu__link ">About</a>
+            </li>
+
+            <li className="menu__items">
+              <a href="#" className="menu__link">Settings</a>
+            </li>
+
+            <li className="menu__items">
+              <a href="#" className="menu__link">Info</a>
+            </li>
+
+            <li>Hello</li>
+
+          </ul>
+        </div>
+      </div>
+
+    </>
+  )
+}
+export default App;
